5516e25ac567f4b481740fd8f3a156fa9fb43107
judooctopus <73967268+judooctopus@users.noreply.github.com>
2021-11-16 17:36:03 -0500

    Initial Commit
    Initial Commit

---
+++ configure-virtualbox.ps1
@@ -0,0 +1,9 @@
+﻿# The below commands will install and configure virtualbox for windows
+# Install Virtualbox
+winget install Oracle.VirtualBox
+
+# Disable the windows 10 hyperviser and allow the built-in virtualbox one to handle it. Needed for VirtualBox 6.1.28
+bcdedit.exe /set hypervisorlauchtype off
+
+#reboot windows
+shutdown /r now
